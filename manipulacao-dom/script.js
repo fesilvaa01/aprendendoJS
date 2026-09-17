bd67075ee037d1d1443909fef5f1d8486fdff49c
FELIPE SILVA DE OLIVEIRA <54016391806@info.local>
2026-09-17 15:46:33 -0300

    :sparkles:feat Continuando o aprendizado do JS

---
+++ manipulacao-dom/script.js
@@ -0,0 +1,91 @@
+
+// const elementos = document.querySelectorAll(".texto");
+
+//* Percorrendo a lista e imprimindo cada um dos elementos
+// elementos.forEach(elemento => console.log(elemento));
+
+//* Percorrendo a lista e pegando o texto do elemento
+// elementos.forEach(elemento => console.log(elemento.innerText));
+
+//* Alterando textos
+// elementos.forEach(elemento => {
+//     elemento.innerText = "Alterado";
+
+//     //elemento.textContent = "Alterado"; //* vem com a formatação do elemento
+// });
+
+//* Trocando as tags - innerHTML
+// elementos.forEach(elemento => {
+//     elemento.innerHTML = "<h2>item</h2>";
+// });
+
+//* Trocando a cor do elemento
+// elementos.forEach(elemento =>{
+//     elemento.style.color = "#0000FF";
+// });
+
+//! EVENTOS COM JS
+//* O que são eventos? Ações do usuário
+
+//* Evento click
+// const botao = document.getElementById("btn");
+
+// botao.addEventListener("click", () => {
+//     alert("Você clicou!");
+// });
+
+//* Evento de digitação (input/keyup)
+
+// const campo = document.getElementById("campo");
+// const resultado = document.getElementById("resultado");
+
+//* INPUT
+//* evento input -> dispara sempre que digita, em tempo real
+// campo.addEventListener("input", () => {
+    
+//     //* value -> o que esta sendo digitado dentro do campo
+//     resultado.innerText = campo.value;
+// })
+
+//* KEYUP
+//* so dispara quando solta a tecla
+//     campo.addEventListener("input", () => {
+//     //* value -> o que esta sendo digitado dentro do campo
+//     resultado.innerText = campo.value;
+// })
+
+//* Evento de mouse
+// const elemento = document.getElementById("troca-cor");
+
+// //* mouseover -> quando passar o mouse pelo elemento
+// elemento.addEventListener("mouseover", () => {
+//     elemento.style.backgroundColor = "#ff0000";
+// })
+
+// //* mouseout -> quando o mouse sai do elemento
+// elemento.addEventListener("mouseout", () => {
+//     //elemento.style.backgroundColor = "#0000ff";
+//     elemento.style.backgroundColor = "";
+// })
+
+//* mousemove
+//* pega a posição do mouse
+// document.addEventListener("mousemove", () => {
+//     console.log('Posição X: ${evento.clienteX}, Posição Y: ${evento.clienteY}');
+    
+// })
+
+//* Evento de Formulário (submit)
+
+const form = document.querySelectorAll("form"); //* pegando direto pela tag form
+
+//* O comportamento padrão do formulario é recarregar a pagina ao enviar o submit
+form.addEventListener("submit", (e) => {
+
+    //* Impede o comportamento padrão do formulario
+    e.preventDefault();
+
+    const nome = document.getElementById("nome").value
+
+    console.log('Nome: ${nome}');
+})
